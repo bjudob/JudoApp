da4a29afbcc4cb9bf327c0e1082f470f271dc566
Boti <konczbotondvagyok@gmail.com>
2018-05-28 13:40:07 +0300

    profile picture change fixed-really! :)

--- app/src/main/java/com/example/botond/judoapp_4/ProfileActivity.java
+++ app/src/main/java/com/example/botond/judoapp_4/ProfileActivity.java
@@ -129,7 +129,8 @@ public class ProfileActivity extends BaseActivity {
         if(user!=null) {
             SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
             String displayname=prefs.getString(displayNameKey, null);
-            if(displayname!=null && displayname!=user.getDisplayName()){
+            
+            if(displayname!=null && !displayname.equals(user.getDisplayName())){
                 saveUserInfo(displayname);
             }
             if(user.getPhotoUrl()!=null){
