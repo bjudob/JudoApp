6d00425fd655f605bc0b007930db45392e7ef90f
Boti <konczbotondvagyok@gmail.com>
2018-05-29 17:00:35 +0300

    Throw class created

---
+++ app/src/main/java/com/example/botond/judoapp_4/domain/Throw.java
@@ -0,0 +1,69 @@
+package com.example.botond.judoapp_4.domain;
+
+import java.util.Objects;
+
+public class Throw {
+
+    private String id;
+    private String name;
+    private String description;
+    private String imageUrl;
+
+    //no args constructor
+    public Throw() {
+    }
+
+    //all args constructor
+    public Throw(String id, String name, String description, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Throw aThrow = (Throw) o;
+        return Objects.equals(id, aThrow.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
