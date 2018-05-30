74d1ba95ec0fab5257e5116b6f2c46396daa435e
Boti <konczbotondvagyok@gmail.com>
2018-05-30 13:47:26 +0300

    BeltsActivity redesigned

--- app/src/main/java/com/example/botond/judoapp_4/ViewBeltActivity.java
+++ app/src/main/java/com/example/botond/judoapp_4/ViewBeltActivity.java
@@ -13,6 +13,6 @@ public class ViewBeltActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_belt);
-        
+
     }
 }
