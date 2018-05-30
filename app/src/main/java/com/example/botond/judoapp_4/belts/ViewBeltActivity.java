02344eaad0c916c5246ed262729e107eb84088fd
Boti <konczbotondvagyok@gmail.com>
2018-05-30 15:09:46 +0300

    ViewBeltActivity gets the parameter and the belt

--- app/src/main/java/com/example/botond/judoapp_4/belts/ViewBeltActivity.java
+++ app/src/main/java/com/example/botond/judoapp_4/belts/ViewBeltActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.botond.judoapp_4.R;
+import com.example.botond.judoapp_4.ctrl.BeltController;
 import com.example.botond.judoapp_4.domain.Belt;
+import com.example.botond.judoapp_4.manager.ResourceManager;
+import com.example.botond.judoapp_4.repo.BeltRepository;
 
 public class ViewBeltActivity extends AppCompatActivity {
 
     private Belt belt;
+    private BeltController beltController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,18 @@ public class ViewBeltActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_belt);
 
         Intent intent = getIntent();
-        String quote = intent.getStringExtra("beltName");
+        String beltName = intent.getStringExtra("beltName");
 
+        beltController= ResourceManager.getBeltController();
+
+        Belt belt=beltController.getByName(beltName);
+
+        if(belt==null){
+            //error, data not found, go back
+            // TODO: 30.05.2018
+        }
+        else{
+
+        }
     }
 }
