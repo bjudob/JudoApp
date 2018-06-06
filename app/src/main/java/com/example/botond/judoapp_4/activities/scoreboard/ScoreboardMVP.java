050e1d9f8faef162e027ff7a18ed5a1185185ac4
Boti <konczbotondvagyok@gmail.com>
2018-06-06 22:04:12 +0300

    scoreboardMVP created

---
+++ app/src/main/java/com/example/botond/judoapp_4/activities/scoreboard/ScoreboardMVP.java
@@ -0,0 +1,10 @@
+package com.example.botond.judoapp_4.activities.scoreboard;
+
+public interface ScoreboardMVP {
+    interface view{
+
+    }
+    interface presenter{
+
+    }
+}
---
+++ app/src/main/java/com/example/botond/judoapp_4/activities/scoreboard/ScoreboardPresenter.java
@@ -0,0 +1,4 @@
+package com.example.botond.judoapp_4.activities.scoreboard;
+
+public class ScoreboardPresenter {
+}
