16d557020f54b443bdb96f446800fd5bd9a22220
Boti <konczbotondvagyok@gmail.com>
2018-06-13 11:43:28 +0300

    profile2 created

---
+++ app/src/main/java/com/example/botond/judoapp_4/activities/profile/Profile2Activity.java
@@ -0,0 +1,15 @@
+package com.example.botond.judoapp_4.activities.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.botond.judoapp_4.R;
+
+public class Profile2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile2);
+    }
+}
