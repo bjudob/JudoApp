5f089830dcd0aaab71b0e010d90d12b3833ffbe7
Boti <konczbotondvagyok@gmail.com>
2018-06-14 14:33:01 +0300

    VocabularyController created

--- app/src/main/java/com/example/botond/judoapp_4/ctrl/VocabularyController.java
+++ app/src/main/java/com/example/botond/judoapp_4/ctrl/VocabularyController.java
@@ -1,4 +1,22 @@
 package com.example.botond.judoapp_4.ctrl;
 
+import com.example.botond.judoapp_4.domain.Vocabulary;
+import com.example.botond.judoapp_4.repo.VocabularyRepository;
+
+import java.util.List;
+
 public class VocabularyController {
+    private VocabularyRepository repo;
+
+    public VocabularyController() {
+        repo=new VocabularyRepository();
+    }
+
+    public Vocabulary getByName(String name){
+        return repo.getByName(name);
+    }
+
+    public List<Vocabulary> getVocabularies(){
+        return repo.getVocabularies();
+    }
 }
