13778deab5db81cdd8a40c9942f9c294bd40fb2b
Boti <konczbotondvagyok@gmail.com>
2018-06-14 14:39:55 +0300

    Vocabularis loading from firebase

--- app/src/main/java/com/example/botond/judoapp_4/manager/ResourceManager.java
+++ app/src/main/java/com/example/botond/judoapp_4/manager/ResourceManager.java
@@ -2,6 +2,7 @@ package com.example.botond.judoapp_4.manager;
 
 import com.example.botond.judoapp_4.ctrl.BeltController;
 import com.example.botond.judoapp_4.ctrl.LectureController;
+import com.example.botond.judoapp_4.ctrl.VocabularyController;
 import com.example.botond.judoapp_4.domain.Vocabulary;
 
 import java.util.List;
@@ -9,12 +10,15 @@ import java.util.List;
 public class ResourceManager {
     public static BeltController beltController;
     public static LectureController lectureController;
+    public static VocabularyController vocabularyController;
 
     public static void init(){
         if(beltController==null)
             beltController=new BeltController();
         if(lectureController==null)
             lectureController=new LectureController();
+        if(vocabularyController==null)
+            vocabularyController=new VocabularyController();
 
     }
 
@@ -25,4 +29,6 @@ public class ResourceManager {
     public static LectureController getLectureController() {
         return lectureController;
     }
+
+    public static VocabularyController getVocabularyController() { return vocabularyController; }
 }
