581a43cedb59072bb11d6ada7ad12fd44b7a96ca
Boti <konczbotondvagyok@gmail.com>
2018-06-24 18:18:45 +0300

    Started work on learnlistfragment

--- app/src/main/java/com/example/botond/judoapp_4/activities/learn/LessonListFragment.java
+++ app/src/main/java/com/example/botond/judoapp_4/activities/learn/LessonListFragment.java
@@ -10,23 +10,10 @@ import android.view.ViewGroup;
 
 import com.example.botond.judoapp_4.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LessonListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LessonListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LessonListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_PARAM_CATEGORY = "paramLessonCategory";
+    
+    private String lessonCategory;
 
     private OnFragmentInteractionListener mListener;
 
@@ -46,8 +33,7 @@ public class LessonListFragment extends Fragment {
     public static LessonListFragment newInstance(String param1, String param2) {
         LessonListFragment fragment = new LessonListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM_CATEGORY, param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +42,7 @@ public class LessonListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            lessonCategory = getArguments().getString(ARG_PARAM_CATEGORY);
         }
     }
 
@@ -66,13 +51,6 @@ public class LessonListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lesson_list, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
@@ -92,18 +70,7 @@ public class LessonListFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void showLecture(String id);
     }
 }
